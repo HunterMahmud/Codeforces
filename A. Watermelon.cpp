13d107009836b36c9d015810cacd3a49d1009866
HunterMahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-04-05 11:23:41 +0600

    Create A. Watermelon.cpp

---
+++ A. Watermelon.cpp
@@ -0,0 +1,11 @@
+// url = https://codeforces.com/problemset/problem/4/A
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	if(n> 3 && !(n%2)) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+	return 0;
+}
