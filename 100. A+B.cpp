b71a640951ba9e7bff597d44f8c2f455aad4f2a2
HunterMahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-04-05 12:29:03 +0600

    Create 100. A+B.cpp

---
+++ 100. A+B.cpp
@@ -0,0 +1,10 @@
+//url = https://codeforces.com/problemsets/acmsguru/problem/99999/100
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b;
+    return 0;
+}
